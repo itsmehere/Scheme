219c18b0d2b39dca342b6af230e53bef61bce468
redrazer <39073078+redrazer@users.noreply.github.com>
2019-08-29 07:26:27 -0700

    Updated factorialV2

--- GeneralExercises/factorialV2.rkt
+++ GeneralExercises/factorialV2.rkt
@@ -2,7 +2,7 @@
 
 (define (factorial n)
   (define (init product x)
-    (if (< x n)
-        (init (* product (+ x 1)) (+ x 1))
+    (if (<= x n)
+        (init (* product x) (+ x 1))
         product))
   (init 1 1))
